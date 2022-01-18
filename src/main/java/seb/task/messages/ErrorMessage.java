9f1eb6413083d75b0004f991b213597443ce5eba
elisiejus <ekumiscius@gmail.com>
2022-01-18 19:34:05 +0200

    Error message update

--- src/main/java/seb/task/messages/ErrorMessage.java
+++ src/main/java/seb/task/messages/ErrorMessage.java
@@ -3,7 +3,9 @@ package seb.task.messages;
 public enum ErrorMessage implements Message {
 
     FAIL_READING_FILE(-0L, "Could not read file!"),
-    NO_FIRST_RESULT(-1L, "Could not find first time result!");
+    NO_FIRST_RESULT(-1L, "Could not find first time result!"),
+    PARSE_ERROR(-2L, "Could not parse athletes results"),
+    FAIL_COUNTING_LINES(-3L, "Could not count file's lines");
 
     private final Long id;
     private final String text;
